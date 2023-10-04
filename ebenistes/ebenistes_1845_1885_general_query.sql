dc0c92aef49cf524848a6decbccd52ecf75e2c2b
JGravier <JGravier@users.noreply.github.com>
2023-10-04 17:52:37 +0200

    update first output

--- ebenistes/ebenistes_1845_1885_general_query.sql
+++ ebenistes/ebenistes_1845_1885_general_query.sql
@@ -6,22 +6,28 @@ WITH per_count AS (
 			SELECT pc.index
 			FROM per_count AS pc
 			WHERE count_ <=1)
-	SELECT DISTINCT e.index, p.ner_xml AS person, act.ner_xml AS activity, s.loc AS loc, s.cardinal AS cardinal,
-	t.ner_xml AS title, e.directory, e.published, lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')) AS fulladd
+	SELECT DISTINCT e.index, TRANSLATE(p.ner_xml,'éëèêàçôö,.:;\-_\(\)\[\]?!$&','eeeeacoo') AS person, TRANSLATE(act.ner_xml,'éëèêàçôö,.:;\-_\(\)\[\]?!$&','eeeeacoo') AS activity, soundex(act.ner_xml) AS actsoundex, s.loc AS loc, s.cardinal AS cardinal,
+	-- change TRANSLATE in unaccent()
+	t.ner_xml AS title, e.directory, e.published, w.liste_type, TRANSLATE(lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')),'éëèêàçôö,.:;\-_\(\)\[\]?!$&','eeeeacoo') AS fulladd, s.index AS id_address
 	FROM short_list AS l
 	INNER JOIN directories.elements AS e ON l.index = e.index
 	INNER JOIN directories.persons AS p ON e.index = p.entry_id
 	INNER JOIN directories.activities AS act ON e.index = act.entry_id
 	INNER JOIN directories.addresses AS s ON e.index = s.entry_id
 	INNER JOIN directories.titles AS t ON e.index = t.entry_id
+	INNER JOIN directories.sources AS w ON e.directory = w.code_fichier 
 	WHERE (
 		(e.published>1844 AND e.published<1886) AND
 		-- Liste des mots-clés: à adapter!
-		(act.ner_xml ILIKE '%ébéniste%' OR 
+		(e.view BETWEEN w.npage_pdf_d AND w.npage_pdf_f) AND
+        (w.liste_type ILIKE '%ListNoms%') AND
+		(act.ner_xml ILIKE '%ébeniste%' OR 
 		act.ner_xml ILIKE '%ebeniste%' OR 
-		act.ner_xml ILIKE '%ébeniste%' OR 
-		act.ner_xml ILIKE '%ebéniste%' OR
-		act.ner_xml ILIKE '%cbeniste%' OR
-		act.ner_xml ILIKE '%cbéniste%')
+		act.ner_xml ILIKE '%ébéniste%' OR 
+		act.ner_xml ILIKE '%ebéniste%' OR 
+		act.ner_xml ILIKE '%cbéniste%' OR 
+		act.ner_xml ILIKE '%bemste%' OR 
+		act.ner_xml ILIKE '%bémste%' OR 
+		 act.ner_xml ILIKE '%cbeniste%')
 		)
 	ORDER BY e.index, e.published ASC;
