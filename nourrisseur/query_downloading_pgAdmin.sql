b55ea1a63cf47ac92d4aaa9c3d54838285fd03e8
JGravier <JGravier@users.noreply.github.com>
2023-10-13 16:12:24 +0200

    data nourrisseur from soduco DB
    direct in pgAdmin

---
+++ nourrisseur/query_downloading_pgAdmin.sql
@@ -0,0 +1,28 @@
+WITH per_count AS (
+			SELECT e.index, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
+			FROM directories.elements AS e
+			ORDER BY count_ DESC
+		), short_list AS ( --Ne conserve que les entrées avec 0 ou 1 PER (au delà, on aura un produit cartésien de tous les attributs)
+			SELECT pc.index
+			FROM per_count AS pc
+			WHERE count_ <=1)
+	SELECT DISTINCT e.index, TRANSLATE(p.ner_xml,'éëèêàçôö,.:;\-_\(\)\[\]?!$&','eeeeacoo') AS person, TRANSLATE(act.ner_xml,'éëèêàçôö,.:;\-_\(\)\[\]?!$&','eeeeacoo') AS activity, soundex(act.ner_xml) AS actsoundex, s.loc AS loc, s.cardinal AS cardinal,
+	-- change TRANSLATE in unaccent()
+	t.ner_xml AS title, e.directory, e.published, w.liste_type, TRANSLATE(lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')),'éëèêàçôö,.:;\-_\(\)\[\]?!$&','eeeeacoo') AS fulladd, s.index AS id_address, g."precise.geo_response" AS precise_geo_response, g."precise.geom" AS precise_geom
+	FROM short_list AS l
+	INNER JOIN directories.elements AS e ON l.index = e.index
+	INNER JOIN directories.persons AS p ON e.index = p.entry_id
+	INNER JOIN directories.activities AS act ON e.index = act.entry_id
+	INNER JOIN directories.addresses AS s ON e.index = s.entry_id
+	INNER JOIN directories.titles AS t ON e.index = t.entry_id
+	INNER JOIN directories.sources AS w ON e.directory = w.code_fichier
+	INNER JOIN directories.geocoding AS g ON e.index = g.entry_id
+	WHERE (
+		-- Liste des mots-clés: à adapter!
+		(e.view BETWEEN w.npage_pdf_d AND w.npage_pdf_f) AND
+        (w.liste_type ILIKE '%ListNoms%') AND
+		((act.ner_xml ILIKE '%nourrisseur%') OR
+		(act.ner_xml ILIKE '%noürrisseur%') OR
+		(act.ner_xml ILIKE '%nourris%'))
+		)
+	ORDER BY e.published ASC;
